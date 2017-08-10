3a01b8021dbcfc733a8a39d12fd4af9bd39792ec
Mikael Walters <mikael.walters@gmail.com>
2017-08-10 16:28:35 -0500

    rough draft of goods.js and added password hash to user.js

---
+++ models/goods.js
@@ -0,0 +1,49 @@
+// Dependencies
+// npm install sequelize-mysql-timestamp
+const TIMESTAMP = require("sequelize-mysql-timestamp")(sequelize);
+
+
+// Creating Goods model
+module.exports = function(sequelize, DataTypes) {
+  var Items = sequelize.define("Items", {
+    itemName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    owner: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    pricePerHour: {
+      type: DataTypes.DECIMAL(6,2),
+      allowNull: false
+    },
+    outTimeStamp: {
+      type: TIMESTAMP,
+      allowNull: false
+    },
+    inTimeStamp: {
+      type: TIMESTAMP,
+      allowNull: false
+    },
+    totalCost: {
+      type: DataTypes.DECIMAL(10,2),
+    },
+    itemPhoto: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    availability: {
+      type: DataTypes.DATE
+    }
+  });
+  return Items;
+};
--- models/user.js
+++ models/user.js
@@ -1,3 +1,11 @@
+// ***************************************************************************************************
+// Requiring bcrypt for password hashing. Using the bcrypt-nodejs version as the regular bcrypt module
+// sometimes causes errors on Windows machines
+var bcrypt = require("bcrypt-nodejs");
+
+// ***************************************************************************************************
+
+// Creating our User model
 module.exports = function(sequelize, DataTypes) {
   var Post = sequelize.define("User", {
     name: {
@@ -53,11 +61,25 @@ module.exports = function(sequelize, DataTypes) {
       }
     }
 
-    
-    
-  
+
+
+
   });
 
+  // Password:
+  // **********************************************************************************************************************************************************************
+
+  // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+  User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  // Hooks are automatic methods that run during various phases of the User Model lifecycle
+  // In this case, before a User is created, we will automatically hash their password
+  User.hook("beforeCreate", function(user) {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+  });
+
+  // **********************************************************************************************************************************************************************
 
   return User;
 };
