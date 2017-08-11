85de00c19c7faf547b8892f9f8c5073c2c18d1dd
Mikael Walters <mikael.walters@gmail.com>
2017-08-11 09:10:06 -0500

    added config.json; modified goods.js, user.js, and server.js

--- server.js
+++ server.js
@@ -1,24 +1,46 @@
-
 var exphbs = require("express-handlebars");
 var express = require("express");
-var app = express();
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+
 var port = process.env.PORT || 3000;
+
+var app = express();
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
+
 // Set Handlebars.
-var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+// Requiring our models
+var db = require("./models");
+
+// Routes
+// ================================================================
+
+
 //require("./views/login.handlebars")(app);
-console.log("logged in");
+// console.log("logged in");
 // Import routes and give the server access to them.
 //var routes = require("./controllers/catsController.js");
 var routes = require("./controllers/goods_controller.js");
 app.use("/", routes);
-app.listen(port);
 
+// Syncing our database and logging a message to the user upon success
+// db.sequelize.sync().then(function() {
+//   app.listen(PORT, function() {
+//     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+//   });
+// });
+
+app.listen(port, function() {
+  console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", port, port);
+});
